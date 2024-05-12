900d3309cd53c2a7d1a434caee0796323eb04a1c
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-05-12 02:03:11 -0400

    Further Control Loop cleanup.

--- src/main/java/frc/robot/controlLoops/velocity/VelocityControlLoop.java
+++ src/main/java/frc/robot/controlLoops/velocity/VelocityControlLoop.java
@@ -1,7 +1,5 @@
 package frc.robot.controlLoops.velocity;
 
-import edu.wpi.first.units.*;
-
 public interface VelocityControlLoop {
 
     double getOutput(double currentVelocity, double nextVelocity);
